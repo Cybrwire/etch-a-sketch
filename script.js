9ab53af6b512b89a451b01bfe47fa19e1668e839
jmontgomery <jmontgomery786@gmail.com>
2024-01-08 21:31:09 -0600

    added resize functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function createGrid(){
+function setGrid(size = 16){
     let gridContainer = document.querySelector('.grid-container');
-    for(let i = 0; i < (16 * 16); i++) {
+    gridContainer.innerHTML = '';
+    for(let i = 0; i < (size * size); i++) {
         
         let div = document.createElement('div');
         div.classList.add("grid-square");
@@ -8,10 +9,12 @@ function createGrid(){
     }
     //grid square width = 960 / 16 (or user selected number)
     document.querySelectorAll('.grid-square').forEach(square => {
-        square.style.width = (960/16) + 'px';
+        square.style.width = (960/size) + 'px';
+        square.style.height = (960/size) + 'px';
     })
-
+    drawPixels();
 }
+
 function drawPixels(){
     document.querySelectorAll('.grid-square').forEach(square => {
         square.addEventListener('mouseover', (e)=>{
@@ -21,5 +24,13 @@ function drawPixels(){
         })
     })
 }
-createGrid();
-drawPixels();
+function changeSize(){
+    let input;
+    do {
+        input = parseInt(prompt("How big do you want the grid to be? Enter a number from 1 - 100: "));
+    }
+    while(input > 100 || input < 1)
+    setGrid(input);
+}
+
+setGrid();
